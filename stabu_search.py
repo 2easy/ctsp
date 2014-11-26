54d3443b514814484645dee2836c9b0a591d0c19
Piotr Olchawa <piotrekolchawa@gmail.com>
2014-11-26 12:03:13 +0100

    update on stabu search

---
+++ stabu_search.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from greedy import greedy_solution
+from random import randint
+
+def mix(t1, t2, solution):
+    # TODO mix 6 elements excluding current ones
+
+def neighbourhood(solution):
+    t1,t2 = sample(range(0,len(solution)))
+    return mix(t1, t2, solution)
