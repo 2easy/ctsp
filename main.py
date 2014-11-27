f124479479d0d7193d5aef1b40688aaa520bc014
Piotr Olchawa <piotrekolchawa@gmail.com>
2014-11-27 11:49:47 +0100

    add simple search - w/o tabu list

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
-from greedy import greedy_solution
-dists = [ [ 10,  0, 1, 2, 3, 4, 5 ],
-          [ 11,  1, 0, 2, 3, 4, 5 ],
-          [ 12,  2, 2, 0, 3, 4, 5 ],
+import greedy
+import stabu_search
+dists = [ [ 10,  0, 10, 20, 3, 4, 5 ],
+          [ 11,  10, 0, 2, 3, 4, 5 ],
+          [ 12,  20, 2, 0, 3, 4, 5 ],
           [ 13,  3, 3, 3, 0, 4, 5 ],
           [ 14,  4, 4, 4, 4, 0, 5 ],
           [ 9,   5, 5, 5, 5, 5, 0 ],
         ]
 
-print(str(greedy_solution(dists)))
+#print(str(greedy.solve(dists)))
+
+print(str(stabu_search.solve(dists)))
--- stabu_search.py
+++ stabu_search.py
@@ -1,10 +1,49 @@
 #!/usr/bin/env python
-from greedy import greedy_solution
-from random import randint
+import greedy
+from helpers import compute_cost
+from random import shuffle
 
-def mix(t1, t2, solution):
-    # TODO mix 6 elements excluding current ones
+def mix(t1, t2):
+    """ mixes two given cycles """
+    new_pairs = []
+    for i in range(len(t1)):
+        for j in range(len(t2)):
+            p1,p2 = t1[:],t2[:]
+            p1[i],p2[j] = t2[j], t1[i]
+            new_pairs.append([p1,p2])
+    return new_pairs
 
+def neighbourhood_mix2t(solution):
+    """ computes neighbourhood of current solution
+        by mixing cities in two randomly choosen tours"""
+    shuffle(solution)
+    t1,t2 = solution.pop(), solution.pop()
+
+    res = []
+    for tp in mix(t1, t2):
+        tmp = solution[:]
+        tmp.extend(tp)
+        res.append(tmp)
+    return res
 def neighbourhood(solution):
-    t1,t2 = sample(range(0,len(solution)))
-    return mix(t1, t2, solution)
+    return neighbourhood_mix2t(solution)
+
+def best_from(solutions, dists):
+    best = solutions.pop()
+    for s in solutions:
+        if compute_cost(s,dists) < compute_cost(best,dists):
+            best = s
+    return best
+
+def solve(dists):
+    i = 0
+    current = greedy.solve(dists)
+    # TODO generalize stop condition by funciton
+    while i < 1000:
+        # TODO include taboo list
+        print(str(current) + " --> " + str(compute_cost(current, dists)))
+        current = best_from(neighbourhood(current), dists)
+        i += 1
+    return current
+#for p in neighbourhood_mix2t([[0,1,2],[3,4,5],[6,7]]):
+#    print(p)
