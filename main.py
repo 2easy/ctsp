4c133d0ded735ea4c21828cf3567edfe2a19fce1
Piotr Olchawa <piotrekolchawa@gmail.com>
2014-12-09 23:08:22 +0100

    change stabu_search into class

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from greedy import Greedy
-import stabu_search
+from stabu_search import SimpleTabooSearch
 import simulated_annealing
 
 # Generate simple example
@@ -11,8 +11,15 @@ dists1 = gen_dists([(0,0), (-4,0), (1,4), (3,4), (3,2), (8,-6), (-2,-3), (-4,-3)
 greedy = Greedy(dists1)
 g_sol = greedy.solve()
 print("GREEDY SOLUTION:\t\t\t"+str(g_sol)+" ---> " + str(greedy.cost))
-sts_sol = stabu_search.solve(dists1)
-print("SIMPLE TABOO SEARCH SOLUTION:\t\t"+ str(sts_sol)+ " ---> "+ str(compute_cost(sts_sol, dists1)))
-sa_sol = simulated_annealing.solve(dists1)
-print("SIMULATED ANNEALING SOLUTION:\t\t"+ str(sa_sol)+ " ---> "+ str(compute_cost(sa_sol, dists1)))
+sts = SimpleTabooSearch(dists1)
+sts_sol = sts.solve()
+print("SIMPLE TABOO SEARCH SOLUTION:\t\t"+ str(sts_sol)+ " ---> "+ str(sts.cost))
+
+sts1 = SimpleTabooSearch(dists1)
+while sts1.step(): pass
+sts1_sol = sts1.solution
+print("SIMPLE TABOO SEARCH STEPbySTEP SOLUTION:\t\t"+ str(sts1_sol)+ " ---> "+ str(sts1.cost))
+
+#sa_sol = simulated_annealing.solve(dists1)
+#print("SIMULATED ANNEALING SOLUTION:\t\t"+ str(sa_sol)+ " ---> "+ str(compute_cost(sa_sol, dists1)))
 
--- stabu_search.py
+++ stabu_search.py
@@ -1,39 +1,64 @@
 #!/usr/bin/env python
-import greedy
+from greedy import Greedy
 from helpers import compute_cost, neighbourhood_mix2t
 
+class SimpleTabooSearch:
+    def __init__(self, dists):
+        self.dists = dists[:]
+        self.i = 0
+        self.taboo = []
+        self.current = Greedy(dists).solve()
+        self.best = self.current[:]
+        self.cost = compute_cost(self.best, self.dists)
+        self.solution = self.best
 
-def neighbourhood(solution):
-    return neighbourhood_mix2t(solution)
+    def neighbourhood(self, solution):
+        return neighbourhood_mix2t(solution)
 
-def exclude(solutions, taboo):
-    res = []
-    for s in solutions:
-        if all([ set(s) != f for f in taboo]):
-            res.append(s)
-    return res
+    def exclude(self, solutions, taboo):
+        res = []
+        for s in solutions:
+            if all([ set(s) != f for f in taboo]):
+                res.append(s)
+        return res
 
-def best_from(solutions, dists, taboo):
-    best = solutions.pop()
-    for s in solutions:
-        if compute_cost(s,dists) < compute_cost(best,dists):
-            best = s
-    taboo.append(set(best))
-    return best,taboo
+    def best_from(self, solutions, dists, taboo):
+        best = solutions.pop()
+        for s in solutions:
+            if compute_cost(s,dists) < compute_cost(best,dists):
+                best = s
+        taboo.append(set(best))
+        return best,taboo
 
-def solve(dists):
-    i,taboo = 0,[]
-    current = greedy.solve(dists)
-    best = current[:]
-    # TODO generalize stop condition by funciton
-    while i < 1000:
-        candidates = exclude(neighbourhood(current), taboo)
-        if candidates:
-            current,taboo = best_from(candidates, dists, taboo)
-            i += 1
+    def step(self):
+        if self.i < 1000:
+            candidates = self.exclude(self.neighbourhood(self.current), self.taboo)
+            if candidates:
+                self.current,self.taboo = self.best_from(candidates, self.dists, self.taboo)
+                self.i += 1
+                # keep the best solution so far
+                if compute_cost(self.current, self.dists) < compute_cost(self.best, self.dists):
+                    self.best = self.current[:]
+            # tell the caller step was evaluated
+            return True
         else:
-            break
-        # keep the best solution so far
-        if compute_cost(current, dists) < compute_cost(best, dists):
-            best = current[:]
-    return best
+            # tell the caller we are already done
+            self.solution = self.best
+            self.cost = compute_cost(self.best, self.dists)
+            return False
+
+    def solve(self):
+        # TODO generalize stop condition by funciton
+        while self.i < 1000:
+            candidates = self.exclude(self.neighbourhood(self.current), self.taboo)
+            if candidates:
+                self.current,self.taboo = self.best_from(candidates, self.dists, self.taboo)
+                self.i += 1
+            else:
+                break
+            # keep the best solution so far
+            if compute_cost(self.current, self.dists) < compute_cost(self.best, self.dists):
+                self.best = self.current[:]
+                self.cost = compute_cost(self.best, self.dists)
+                self.solution = self.best
+        return self.best
