acf365b76e87938a8d76b6f18f60f6d02b8643e7
Piotr Olchawa <piotrekolchawa@gmail.com>
2015-01-19 17:30:18 +0100

    add heating up to SA

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -2,7 +2,7 @@
 
 from greedy import Greedy 
 from helpers import compute_cost, neighbourhood_mix2t
-from random import choice, random
+from random import choice, random, randint
 from math import exp
 
 # TODO choose start temp wiser, by experiment
@@ -13,12 +13,17 @@ class SimulatedAnnealing:
         self.dists = dists[:]
         self.best = self.current = self.last = init_solution
         self.cost = compute_cost(self.best, self.dists)
-        
+
     def neighbourhood(self,solution):
         return neighbourhood_mix2t(solution)
 
     def update_temp(self,temp, i):
-        return temp - i
+        # heat up from time to time, so that we can escape the local minima
+        if i % 100 == 0 and random() > 0.5:
+            # heat up by random amount between two arbitrarily numbers
+            return temp + randint(17,163)
+        else:
+            return temp - i
 
     def solve(self, it):
         i = 0
@@ -27,19 +32,19 @@ class SimulatedAnnealing:
         while i < 1000:
             self.current = choice(self.neighbourhood(self.current))
             dcost = float(compute_cost(self.current, self.dists) - compute_cost(self.last, self.dists))
-            
+
             if compute_cost(self.best, self.dists) > compute_cost(self.current, self.dists):
                 self.best = self.current
                 self.cost = compute_cost(self.best, self.dists)
-            
+
             if dcost < 0:
                 self.last = self.current
             else:
                 if random() >= exp(-(dcost/temp)): # SA secret formula
                     self.last = self.current
-                else: 
+                else:
                     self.current = self.last
-                    
+
             temp = self.update_temp(temp, i)
             i += 1
         if it == 0 :
